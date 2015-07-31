b060c9845ce99c3a25d26c6db5491da35d40f718
Kevin Gautreau <kevin@kgaut.net>
2015-07-31 19:13:31 +0200

    block - scaffolding

---
+++ src/Plugin/Block/NextBets.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\mespronos\Plugin\Block\NextBets.
+ */
+
+namespace Drupal\mespronos\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'NextBets' block.
+ *
+ * @Block(
+ *  id = "next_bets",
+ *  admin_label = @Translation("next_bets"),
+ * )
+ */
+class NextBets extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['number_of_days_to_display'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Number of days to display'),
+      '#description' => $this->t(''),
+      '#default_value' => isset($this->configuration['number_of_days_to_display']) ? $this->configuration['number_of_days_to_display'] : 5,
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['number_of_days_to_display'] = $form_state->getValue('number_of_days_to_display');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['next_bets_number_of_days_to_display']['#markup'] = '<p>' . $this->configuration['number_of_days_to_display'] . '</p>';
+
+    return $build;
+  }
+
+}
