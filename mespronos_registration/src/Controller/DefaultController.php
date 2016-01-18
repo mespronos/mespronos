7deac02e011935fb627f623da515084a970aca25
Kevin Gautreau <contact@kgaut.net>
2016-01-18 14:33:56 +0100

    Registration - setup join page

--- mespronos_registration/src/Controller/DefaultController.php
+++ mespronos_registration/src/Controller/DefaultController.php
@@ -22,10 +22,37 @@ class DefaultController extends ControllerBase {
    *   Return Hello string.
    */
   public function join() {
+    $login_form = self::getLoginForm();
+    $registration_form = self::getRegistrationForm();
+
     return [
         '#theme' => 'join',
-        '#test_var' => 'looool'
+        '#test_var' => 'looool',
+        '#login_form' => render($login_form),
+        '#registration_form' => render($registration_form),
     ];
   }
 
+  public static function getLoginForm() {
+    $login_form = \Drupal::formBuilder()
+      ->getForm('\Drupal\user\Form\UserLoginForm');
+    $login_form['name']['#description'] = '';
+    $login_form['pass']['#description'] = '';
+    return $login_form;
+  }
+
+  public static function getRegistrationForm() {
+    $account = \Drupal::entityManager()->getStorage('user') ->create(array());
+    $form =\Drupal::service('entity.form_builder')->getForm($account, 'default');
+    dpm($form);
+    $form['status']['#access'] = false;
+    $form['roles']['#access'] = false;
+    $form['notify']['#access'] = false;
+    $form['user_picture']['#access'] = false;
+    $form['contact']['#access'] = false;
+    $form['timezone']['#access'] = false;
+    ///$form
+    return $form;
+
+  }
 }
