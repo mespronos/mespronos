12551c100ccda9d604a013b976e15f5b4dcd2d66
Kevin Gautreau <contact@kgaut.net>
2016-01-19 09:20:49 +0100

    Next bets - create route and controller method cf #12

--- src/Controller/BettingController.php
+++ src/Controller/BettingController.php
@@ -9,8 +9,12 @@ namespace Drupal\mespronos\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\mespronos\Entity\Controller\GameController;
+use Drupal\mespronos\Entity\Controller\DayController;
 use Drupal\mespronos\Entity\Controller\UserInvolveController;
+use Drupal\mespronos\Entity\League;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * Class DefaultController.
@@ -28,6 +32,62 @@ class BettingController extends ControllerBase {
     return [
         '#type' => 'markup',
         '#markup' => $this->t('Hello World!', [])
+    ];
+  }
+
+  public function nextBets() {
+    $user = \Drupal::currentUser();
+    $user_uid =  $user->id();
+    $days = DayController::getNextDaysToBet(10);
+    foreach ($days  as $day_id => $day) {
+      $league_id = $day->entity->get('league')->first()->getValue()['target_id'];
+      if(!isset($leagues[$league_id])) {
+        $leagues[$league_id] = League::load($league_id);
+      }
+      $league = $leagues[$league_id];
+      if(!isset($user_involvements[$league_id])) {
+        $user_involvements[$league_id] = UserInvolveController::isUserInvolve($user_uid ,$league_id);
+      }
+      $day->involve = $user_involvements[$league_id];
+
+      $game_date = \DateTime::createFromFormat('Y-m-d\TH:i:s',$day->day_date);
+      $now_date = new \DateTime();
+
+      $i = $game_date->diff($now_date);
+
+      if($day->involve) {
+        $action_links = Link::fromTextAndUrl(
+          $this->t('Bet now'),
+          new Url('mespronos.day.bet', array('day' => $day_id))
+        );
+      }
+      else {
+        $action_links = Link::fromTextAndUrl(
+          $this->t('Start betting now !'),
+          new Url('mespronos.league.register', array('league' => $league->id()))
+        );
+      }
+      $row = [
+        $league->label(),
+        $day->entity->label(),
+        $day->nb_game,
+
+        $i->format('%a') >0 ? $this->t('@d days, @GH@im',array('@d'=>$i->format('%a'),'@G'=>$i->format('%H'),'@i'=>$i->format('%i'))) : $this->t('@GH@im',array('@G'=>$i->format('%H'),'@i'=>$i->format('%i'))),
+        $action_links,
+      ];
+      $rows[] = $row;
+    }
+    $header = [
+      $this->t('League',array(),array('context'=>'mespronos')),
+      $this->t('Day',array(),array('context'=>'mespronos')),
+      $this->t('Bets left',array(),array('context'=>'mespronos')),
+      $this->t('Time left',array(),array('context'=>'mespronos')),
+      '',
+    ];
+    return [
+      '#theme' => 'table',
+      '#rows' => $rows,
+      '#header' => $header,
     ];
   }
 
