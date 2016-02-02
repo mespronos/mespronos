4889e04117d8b28c26336545e89e077113a2185f
Kevin Gautreau <contact@kgaut.net>
2016-02-02 07:44:26 +0100

    MPNContentEntityBase - manage case where owner is 'better' or 'user_id'

--- src/Entity/MPNContentEntityBase.php
+++ src/Entity/MPNContentEntityBase.php
@@ -19,23 +19,43 @@ abstract class MPNContentEntityBase extends ContentEntityBase {
    * @return UserInterface
    */
   public function getOwner() {
-    return $this->get('user_id')->entity;
+    if(self::hasField('better')) {
+      return $this->get('better')->entity;
+    }
+    else {
+      return $this->get('user_id')->entity;
+    }
   }
 
   /**
    * @return integer
    */
   public function getOwnerId() {
-    return $this->get('better')->target_id;
+    if(self::hasField('better')) {
+      return $this->get('better')->target_id;
+    }
+    else {
+      return $this->get('user_id')->target_id;
+    }
   }
 
   public function setOwner(UserInterface $account) {
-    $this->set('better', $account->id());
+    if(self::hasField('better')) {
+      $this->set('better', $account->id());
+    }
+    else {
+      $this->set('user_id', $account->id());
+    }
     return $this;
   }
 
   public function setOwnerId($uid) {
-    $this->set('user_id', $uid);
+    if(self::hasField('better')) {
+      $this->set('better', $uid);
+    }
+    else {
+      $this->set('user_id', $uid);
+    }
     return $this;
   }
 
