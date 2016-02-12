2d8f002d2689efbe0f91b31c8b24e7006e4334e9
Kevin Gautreau <contact@kgaut.net>
2016-02-12 16:06:55 +0100

    Bet - on winner - define betting page - close #50

--- src/Form/GamesBetting.php
+++ src/Form/GamesBetting.php
@@ -29,6 +29,7 @@ class GamesBetting extends FormBase {
     $league = $day->getLeague();
     $games = GameController::getGamesToBet($day);
 
+    $betting_type = $league->getBettingType(TRUE);
 
     $form['infos'] = array(
       '#type' => 'container',
@@ -78,24 +79,47 @@ class GamesBetting extends FormBase {
         '#type' => 'hidden',
         '#value' => $bet->id(),
       );
-      $form['games'][$game->id()]['score_team_1'] = array(
-        '#type' => 'textfield',
-        '#size' => '5',
-        '#default_value' => $bet->getScoreTeam1(),
-        '#title' => $game->get('team_1')->entity->label(),
-        '#attributes' => array(
-          'class' => array('team_1')
-        )
-      );
-      $form['games'][$game->id()]['score_team_2'] = array(
-        '#type' => 'textfield',
-        '#size' => '5',
-        '#default_value' => $bet->getScoreTeam2(),
-        '#title' => $game->get('team_2')->entity->label(),
-        '#attributes' => array(
-          'class' => array('team_2')
-        )
-      );
+      if($betting_type == 'score') {
+        $form['games'][$game->id()]['score_team_1'] = array(
+          '#type' => 'textfield',
+          '#size' => '5',
+          '#default_value' => $bet->getScoreTeam1(),
+          '#title' => $game->get('team_1')->entity->label(),
+          '#attributes' => array(
+            'class' => array('team_1')
+          )
+        );
+        $form['games'][$game->id()]['score_team_2'] = array(
+          '#type' => 'textfield',
+          '#size' => '5',
+          '#default_value' => $bet->getScoreTeam2(),
+          '#title' => $game->get('team_2')->entity->label(),
+          '#attributes' => array(
+            'class' => array('team_2')
+          )
+        );
+      }
+      else {
+        $form['games'][$game->id()]['winner'] = [
+          '#type' => 'radios',
+          '#options' => [
+            '1' => $game->get('team_1')->entity->label(),
+            'N' => t('Draw'),
+            '2' => $game->get('team_2')->entity->label(),
+          ]
+        ];
+        if(!is_null($bet->getScoreTeam1()) && !is_null($bet->getScoreTeam2())) {
+          if($bet->getScoreTeam1() == $bet->getScoreTeam2()) {
+            $form['games'][$game->id()]['winner']['#default_value'] = 'N';
+          }
+          elseif($bet->getScoreTeam1() > $bet->getScoreTeam2()) {
+            $form['games'][$game->id()]['winner']['#default_value'] = '1';
+          }
+          elseif($bet->getScoreTeam1() < $bet->getScoreTeam2()) {
+            $form['games'][$game->id()]['winner']['#default_value'] = '2';
+          }
+        }
+      }
     }
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = array(
@@ -108,12 +132,17 @@ class GamesBetting extends FormBase {
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $games = $form_state->getValue('games');
+    $day = $this->extractDay($form_state);
+    $league = $day->getLeague();
+    $betting_type = $league->getBettingType(TRUE);
     foreach($games as $game_id => $game_data) {
-      if ($game_data['score_team_1'] != '' && $game_data['score_team_1'] < 0) {
-        $form_state->setErrorByName('games][' . $game_id . '][score_team_1', $this->t("Can't be less than 0."));
-      }
-      if ($game_data['score_team_2'] != '' && $game_data['score_team_2'] < 0) {
-        $form_state->setErrorByName('games][' . $game_id . '][score_team_2', $this->t("Can't be less than 0."));
+      if($betting_type == 'score') {
+        if ($game_data['score_team_1'] != '' && $game_data['score_team_1'] < 0) {
+          $form_state->setErrorByName('games][' . $game_id . '][score_team_1', $this->t("Can't be less than 0."));
+        }
+        if ($game_data['score_team_2'] != '' && $game_data['score_team_2'] < 0) {
+          $form_state->setErrorByName('games][' . $game_id . '][score_team_2', $this->t("Can't be less than 0."));
+        }
       }
     }
   }
@@ -126,27 +155,50 @@ class GamesBetting extends FormBase {
     $user_id = $form_state->getValue('user');
     $user_storage = \Drupal::entityManager()->getStorage('user');
     $user = $user_storage->load($user_id);
+    $day = $this->extractDay($form_state);
+    $league = $day->getLeague();
+    $betting_type = $league->getBettingType(TRUE);
     $i = 0;
     $j = 0;
-    foreach($games as $game_id => $game_data) {
+    foreach($games as $game_id => &$game_data) {
       $bet_storage = \Drupal::entityManager()->getStorage('bet');
-      if($game_data['score_team_1'] != '' && $game_data['score_team_2'] != '' && $game_id == $game_data['token_id']) {
-        if($game_data['bet_id'] !== null) {
-          $bet = $bet_storage->load($game_data['bet_id']);
+      if($game_id == $game_data['token_id']) {
+        if($betting_type == 'winner') {
+          if($game_data['winner'] != '' && in_array($game_data['winner'],array('1','N','2'))) {
+            switch ($game_data['winner']) {
+              case '1' :
+                $game_data['score_team_1'] = 1;
+                $game_data['score_team_2'] = 0;
+                break;
+              case '2' :
+                $game_data['score_team_1'] = 0;
+                $game_data['score_team_2'] = 1;
+                break;
+              case 'N' :
+                $game_data['score_team_1'] = 0;
+                $game_data['score_team_2'] = 0;
+                break;
+            }
+          }
         }
-        else {
-          $bet = $bet_storage->create(array());
-          $bet->set('game',$game_id);
-          $bet->set('better',$user->id());
-        }
-        $bet->set('score_team_1',$game_data['score_team_1']);
-        $bet->set('score_team_2',$game_data['score_team_2']);
-        if($bet->isAllowed()) {
-          $bet->save();
-          $i++;
-        }
-        else {
-          $j++;
+        if(isset($game_data['score_team_1']) && isset($game_data['score_team_2']) && is_numeric($game_data['score_team_1']) && is_numeric($game_data['score_team_2'])) {
+          if($game_data['bet_id'] !== null) {
+            $bet = $bet_storage->load($game_data['bet_id']);
+          }
+          else {
+            $bet = $bet_storage->create(array());
+            $bet->set('game',$game_id);
+            $bet->set('better',$user->id());
+          }
+          $bet->set('score_team_1',$game_data['score_team_1']);
+          $bet->set('score_team_2',$game_data['score_team_2']);
+          if($bet->isAllowed()) {
+            $bet->save();
+            $i++;
+          }
+          else {
+            $j++;
+          }
         }
       }
     }
@@ -160,7 +212,7 @@ class GamesBetting extends FormBase {
 
   /**
    * @param $form_state
-   * @return Day
+   * @return \Drupal\mespronos\Entity\Day
    */
   protected function extractDay(FormStateInterface $form_state) {
     return $form_state->getBuildInfo()['args'][0];
