8ae19cca8515494828a661d03864800b5bb12cdb
Kevin Gautreau <contact@kgaut.net>
2016-02-12 10:56:09 +0100

    RankingLeague - add unit test

---
+++ src/Tests/MespronosRankingLeagueTest.php
@@ -0,0 +1,254 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\mespronos\Tests\MespronosLeagueTest.
+ */
+
+namespace Drupal\mespronos\Tests;
+
+use Drupal\mespronos\Entity\RankingDay;
+use Drupal\mespronos\Controller\RankingController;
+use Drupal\mespronos\Entity\RankingLeague;
+use Drupal\simpletest\WebTestBase;
+use Drupal\mespronos\Entity\Sport;
+use Drupal\mespronos\Entity\League;
+use Drupal\mespronos\Entity\Team;
+use Drupal\mespronos\Entity\Day;
+use Drupal\mespronos\Entity\Game;
+use Drupal\mespronos\Entity\Bet;
+use MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__\stdClass;
+
+/**
+ * Provides automated tests for the mespronos module.
+ * @group mespronos
+ */
+class MespronosRankingLeagueTest extends WebTestBase {
+  public $sport;
+  public $league1;
+  public $league2;
+  public $league1day1;
+  public $league1day2;
+  public $league2day1;
+  public $league2day2;
+  public $team1;
+  public $team2;
+  public $team3;
+  public $team4;
+  public $l1d1game1;
+  public $l1d2game2;
+  public $l2d1game3;
+  public $l2d2game4;
+  public $better1;
+  public $better2;
+  public $better3;
+  public $better4;
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => "MesPronos RankingDay functionality",
+      'description' => 'Test Unit for user permissions.',
+      'group' => 'MesPronos',
+    );
+  }
+
+  static public $modules = array(
+    'mespronos',
+  );
+
+  public function setUp() {
+    parent::setUp();
+    $this->sport = Sport::create(array(
+      'name' => 'Football',
+    ));
+    $this->sport->save();
+
+    $this->league1 = League::create(array(
+      'sport' => $this->sport->id(),
+      'name' => 'test championnat 1',
+      'betting_type' => 'score',
+      'classement' => true,
+      'status' => 'active',
+    ));
+    $this->league1->save();
+
+    $this->league2 = League::create(array(
+      'sport' => $this->sport->id(),
+      'name' => 'test championnat 2',
+      'betting_type' => 'score',
+      'classement' => true,
+      'status' => 'active',
+    ));
+    $this->league2->save();
+
+    $this->team1 = Team::create(['name' => 'team1']);
+    $this->team1->save();
+    $this->team2 = Team::create(['name' => 'team2']);
+    $this->team2->save();
+    $this->team3 = Team::create(['name' => 'team3']);
+    $this->team3->save();
+    $this->team4 = Team::create(['name' => 'team4']);
+    $this->team4->save();
+
+
+    $this->league1day1 = Day::create(array(
+      'league' => $this->league1->id(),
+      'number' => 1,
+    ));
+    $this->league1day1->save();
+
+    $this->league1day2 = Day::create(array(
+      'league' => $this->league1->id(),
+      'number' => 2,
+    ));
+    $this->league1day2->save();
+
+    $this->league2day1 = Day::create(array(
+      'league' => $this->league2->id(),
+      'number' => 1,
+    ));
+    $this->league2day1->save();
+
+    $this->league2day2 = Day::create(array(
+      'league' => $this->league2->id(),
+      'number' => 2,
+    ));
+    $this->league2day2->save();
+
+    $this->better1 = $this->drupalCreateUser();
+    $this->better2 = $this->drupalCreateUser();
+    $this->better3 = $this->drupalCreateUser();
+    $this->better4 = $this->drupalCreateUser();
+
+    $dateO = new \DateTime();
+    $date = $dateO->format('Y-m-d\TH:i:s');
+
+    $this->l1d1game1 = Game::create(array(
+      'team_1' => $this->team1->id(),
+      'team_2' => $this->team2->id(),
+      'day' => $this->league1day1->id(),
+      'game_date' => $date,
+    ));
+    $this->l1d1game1->save();
+
+    $this->l1d2game2 = Game::create(array(
+      'team_1' => $this->team3->id(),
+      'team_2' => $this->team4->id(),
+      'day' => $this->league1day2->id(),
+      'game_date' => $date,
+    ));
+    $this->l1d2game2->save();
+
+    $this->l2d1game3 = Game::create(array(
+      'team_1' => $this->team1->id(),
+      'team_2' => $this->team2->id(),
+      'day' => $this->league2day1->id(),
+      'game_date' => $date,
+    ));
+    $this->l2d1game3->save();
+
+    $this->l2d2game4 = Game::create(array(
+      'team_1' => $this->team3->id(),
+      'team_2' => $this->team4->id(),
+      'day' => $this->league2day2->id(),
+      'game_date' => $date,
+    ));
+    $this->l2d2game4->save();
+  }
+
+  public function testSimpleWithOnlyOneDay() {
+    $betsDay1 = [];
+    $betsDay1[] = Bet::create(array(
+      'better' => $this->better1->id(),
+      'game' => $this->l1d1game1->id(),
+      'score_team_1' => 1,
+      'score_team_2' => 1,
+    ));
+    $betsDay1[] = Bet::create(array(
+      'better' => $this->better2->id(),
+      'game' => $this->l1d1game1->id(),
+      'score_team_1' => 1,
+      'score_team_2' => 1,
+    ));
+    $betsDay1[] = Bet::create(array(
+      'better' => $this->better3->id(),
+      'game' => $this->l1d1game1->id(),
+      'score_team_1' => 1,
+      'score_team_2' => 1,
+    ));
+    $betsDay1[] = Bet::create(array(
+      'better' => $this->better4->id(),
+      'game' => $this->l1d1game1->id(),
+      'score_team_1' => 1,
+      'score_team_2' => 1,
+    ));
+
+    foreach($betsDay1 as $bet) {
+      $bet->save();
+    }
+
+    $this->l1d1game1->setScore(1,1)->save();
+
+    $this->assertTrue($this->l1d1game1->isScoreSetted(),t('Game1 score is setted'));
+
+    foreach($betsDay1 as $bet) {
+      $bet = Bet::load($bet->id());
+      $this->assertEqual($bet->getPoints(),10,t('good bets worth 10 points'));
+    }
+
+    RankingDay::createRanking($this->league1day1);
+    $ranking_day_1 = RankingDay::getRankingForDay($this->league1day1);
+
+    $this->assertEqual(count($ranking_day_1),4,t('Day 1 : @nb betters, so ranking contains @nb lines',array('@nb'=>count($betsDay1))));
+
+    RankingLeague::createRanking($this->league1);
+    $ranking_league_1 = RankingLeague::getRankingForLeague($this->league1);
+
+    $this->assertEqual(count($ranking_league_1),4,t('League 1 : @nb betters, so ranking contains @nb lines',array('@nb'=>count($betsDay1))));
+
+    //DAY 2
+    $betsDay2 = [];
+    $betsDay2[] = Bet::create(array(
+      'better' => $this->better1->id(),
+      'game' => $this->l1d2game2->id(),
+      'score_team_1' => 1,
+      'score_team_2' => 1,
+    ));
+    $betsDay2[] = Bet::create(array(
+      'better' => $this->better2->id(),
+      'game' => $this->l1d2game2->id(),
+      'score_team_1' => 1,
+      'score_team_2' => 1,
+    ));
+    $betsDay2[] = Bet::create(array(
+      'better' => $this->better3->id(),
+      'game' => $this->l1d2game2->id(),
+      'score_team_1' => 1,
+      'score_team_2' => 1,
+    ));
+
+    foreach($betsDay2 as $bet) {
+      $bet->save();
+    }
+
+    $this->l1d2game2->setScore(1,1)->save();
+
+    $this->assertTrue($this->l1d2game2->isScoreSetted(),t('Game1 score is setted'));
+
+    foreach($betsDay2 as $bet) {
+      $bet = Bet::load($bet->id());
+      $this->assertEqual($bet->getPoints(),10,t('good bets worth 10 points'));
+    }
+
+    RankingDay::createRanking($this->league1day2);
+    $ranking_day_1 = RankingDay::getRankingForDay($this->league1day2);
+
+    $this->assertEqual(count($ranking_day_1),count($betsDay2),t('Day 1 : @nb betters, so ranking contains @nb lines',array('@nb'=>count($betsDay2))));
+
+    RankingLeague::createRanking($this->league1);
+    $ranking_league_1 = RankingLeague::getRankingForLeague($this->league1);
+    $this->assertEqual(count($ranking_league_1),max(count($betsDay2),count($betsDay1)),t('League 1 : @nb betters, so ranking contains @nb lines',array('@nb'=>max(count($betsDay2),count($betsDay1)))));
+  }
+}
