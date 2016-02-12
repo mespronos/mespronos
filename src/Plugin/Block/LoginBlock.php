161095c8c807b145fb0c2d1104e1625c4c3db251
Kevin Gautreau <contact@kgaut.net>
2016-02-12 17:38:13 +0100

    User - link password reset page from login block

--- src/Plugin/Block/LoginBlock.php
+++ src/Plugin/Block/LoginBlock.php
@@ -31,10 +31,16 @@ class LoginBlock extends BlockBase {
     $login_form['name']['#description'] = '';
     $login_form['pass']['#description'] = '';
     $login_form['no_account'] = [
-      '#markup' => Link::fromTextAndUrl(
+      '#markup' => '<p>'.Link::fromTextAndUrl(
         t('No account ? Register now !'),Url::fromRoute('mespronos.join',[])
-      )->toString(),
+      )->toString().'</p>',
       '#weight' => 100,
+    ];
+    $login_form['password_reset'] = [
+      '#markup' => '<p>'.Link::fromTextAndUrl(
+        t('Forget your password ?'),Url::fromRoute('mespronos.password-reset',[])
+      )->toString().'</p>',
+      '#weight' => 101,
     ];
     return $login_form;
   }
