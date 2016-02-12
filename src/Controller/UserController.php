b14228a2a002120fe86260cdc9810f96e387fce0
Kevin Gautreau <contact@kgaut.net>
2016-02-12 17:44:36 +0100

    User - Create a user login page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,8 @@
 namespace Drupal\mespronos\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * Class DefaultController.
@@ -34,6 +36,11 @@ class UserController extends ControllerBase {
     return $registration_form;
   }
 
+  public function login() {
+    $login_form = self::getLoginForm();
+    return $login_form;
+  }
+
   public static function getRegistrationForm() {
     $account = \Drupal::entityManager()->getStorage('user') ->create(array());
     $form =\Drupal::service('entity.form_builder')->getForm($account, 'default');
@@ -56,5 +63,25 @@ class UserController extends ControllerBase {
     unset($password_reset_form['mail']);
     $password_reset_form['actions']['submit']['#value'] = t('Send me password reset instructions');
     return $password_reset_form;
+  }
+
+  public static function getLoginForm() {
+    $login_form = \Drupal::formBuilder()
+      ->getForm('\Drupal\user\Form\UserLoginForm');
+    $login_form['name']['#description'] = '';
+    $login_form['pass']['#description'] = '';
+    $login_form['no_account'] = [
+      '#markup' => '<p>'.Link::fromTextAndUrl(
+          t('No account ? Register now !'),Url::fromRoute('mespronos.join',[])
+        )->toString().'</p>',
+      '#weight' => 100,
+    ];
+    $login_form['password_reset'] = [
+      '#markup' => '<p>'.Link::fromTextAndUrl(
+          t('Forget your password ?'),Url::fromRoute('mespronos.password-reset',[])
+        )->toString().'</p>',
+      '#weight' => 101,
+    ];
+    return $login_form;
   }
 }
--- src/Plugin/Block/LoginBlock.php
+++ src/Plugin/Block/LoginBlock.php
@@ -8,8 +8,7 @@
 namespace Drupal\mespronos\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Url;
-use Drupal\Core\Link;
+use Drupal\mespronos\Controller\UserController;
 
 /**
  * Provides a 'LoginBlock' block.
@@ -26,22 +25,7 @@ class LoginBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $login_form = \Drupal::formBuilder()
-      ->getForm('\Drupal\user\Form\UserLoginForm');
-    $login_form['name']['#description'] = '';
-    $login_form['pass']['#description'] = '';
-    $login_form['no_account'] = [
-      '#markup' => '<p>'.Link::fromTextAndUrl(
-        t('No account ? Register now !'),Url::fromRoute('mespronos.join',[])
-      )->toString().'</p>',
-      '#weight' => 100,
-    ];
-    $login_form['password_reset'] = [
-      '#markup' => '<p>'.Link::fromTextAndUrl(
-        t('Forget your password ?'),Url::fromRoute('mespronos.password-reset',[])
-      )->toString().'</p>',
-      '#weight' => 101,
-    ];
+    $login_form = UserController::getLoginForm();
     return $login_form;
   }
 
