3577e526845ef27072bd94061df27f084b5a911d
Kevin Gautreau <contact@kgaut.net>
2016-02-23 17:27:06 +0100

    Breadcrumb - bet

---
+++ src/Breadcrumb/BetBreadcrumbBuilder.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\mespronos\Breadcrumb\BetBreadcrumbBuilder.
+ */
+
+namespace Drupal\mespronos\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Breadcrumb\Breadcrumb;
+
+class BetBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+    public function applies(RouteMatchInterface $route_match) {
+        return $route_match->getCurrentRouteMatch()->getRouteName() == 'mespronos.day.bet';
+    }
+
+    public function build(RouteMatchInterface $route_match) {
+        /** @var \Drupal\mespronos\Entity\Day $day */
+        $day = $route_match->getParameter('day');
+        $league = $day->getLeague();
+        $breadcrumb = new Breadcrumb();
+        $breadcrumb->addCacheContexts(['route']);
+        $links = [];
+        $links[] = Link::createFromRoute(t('Home'), '<front>');
+        $links[] = Link::createFromRoute(t('Leagues'), 'mespronos.leagues.list');
+        $links[] = Link::createFromRoute($league->label(), 'mespronos.league.index',['league'=>$league->id()]);
+        return $breadcrumb->setLinks($links);
+    }
+
+}
