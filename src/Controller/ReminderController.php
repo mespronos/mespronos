dfca6d71e533fadd75a8968ccca6e964c85a5e0e
Kevin Gautreau <contact@kgaut.net>
2016-03-09 18:52:56 +0100

    reminder - scaffold

---
+++ src/Controller/ReminderController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\mespronos\Controller\ReminderController.
+ */
+
+namespace Drupal\mespronos\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class ReminderController.
+ *
+ * @package Drupal\mespronos\Controller
+ */
+class ReminderController extends ControllerBase {
+
+}
---
+++ src/Tests/MespronosReminderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\mespronos\Tests\MespronosReminderTest.
+ */
+
+namespace Drupal\mespronos\Tests;
+
+use Drupal\simpletest\WebTestBase;
+use Drupal\mespronos\Entity\Sport;
+use Drupal\mespronos\Entity\League;
+use Drupal\mespronos\Entity\Team;
+
+/**
+ * Provides automated tests for the mespronos module.
+ * @group mespronos
+ */
+class MespronosReminderTest extends WebTestBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => "MesPronos Reminder functionality",
+      'description' => 'Test Unit for reminder features.',
+      'group' => 'MesPronos',
+    );
+  }
+
+  static public $modules = array(
+    'mespronos',
+  );
+
+}
