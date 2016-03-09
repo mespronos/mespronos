900540997533fa63579c82301e1cb9ad501b5c70
Kevin Gautreau <contact@kgaut.net>
2016-03-09 19:14:26 +0100

    Reminder - create an administration form

--- src/Form/AdministrationConfigForm.php
+++ src/Form/AdministrationConfigForm.php
@@ -2,38 +2,62 @@
 
 namespace Drupal\mespronos\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
-/**
- * Implements an example form.
- */
-class AdministrationConfigForm extends FormBase {
 
-  /**
-   * {@inheritdoc}.
-   */
+class AdministrationConfigForm extends ConfigFormBase {
+
   public function getFormId() {
     return 'administration_config_form';
   }
 
-  /**
-   * {@inheritdoc}.
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  protected function getEditableConfigNames() {
+    return [
+      'mespronos.reminder'
+    ];
+  }
 
-    return $form;
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['reminder'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Reminder'),
+    ];
+
+    $form['reminder']['reminder_enabled'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable reminder notifications'),
+      '#description' => $this->t(''),
+      '#default_value' => $this->config('mespronos.reminder')->get('enabled'),
+    ];
+
+    $form['reminder']['reminder_hour'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Number of hours for notifications'),
+      '#description' => $this->t(''),
+      '#default_value' => $this->config('mespronos.reminder')->get('hours'),
+      '#states' => [
+        'required' => [':input[name="reminder_enabled"]' => ['checked' => true]],
+        'visible' =>[':input[name="reminder_enabled"]' => ['checked' => true]],
+      ]
+    ];
+
+    return parent::buildForm($form, $form_state);
   }
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
-
+    if ($form_state->getValue('reminder_hour') <= 0) {
+      $form_state->setErrorByName('reminder_hour', $this->t("Can't be less or equal to 0."));
+    }
+    if (intval($form_state->getValue('reminder_hour')) != $form_state->getValue('reminder_hour')) {
+      $form_state->setErrorByName('reminder_hour', $this->t("Should be a integer"));
+    }
   }
 
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
+    $this->config('mespronos.reminder')
+      ->set('enabled', (string) $form_state->getValue('reminder_enabled'))
+      ->set('hours', (string) $form_state->getValue('reminder_hour'))
+      ->save(TRUE);
   }
-
 }
