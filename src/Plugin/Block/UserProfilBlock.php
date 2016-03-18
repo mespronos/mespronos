976b487948d7c77275c9ec19da810344cb2cba28
Kevin Gautreau <contact@kgaut.net>
2016-03-18 10:33:53 +0100

    Profile page - create block

---
+++ src/Plugin/Block/UserProfilBlock.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mespronos\Plugin\Block\UserProfilBlock.
+ */
+
+namespace Drupal\mespronos\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\mespronos\Controller\UserController;
+use Drupal\mespronos\Entity\RankingGeneral;
+use Drupal\user\Entity\User;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+
+/**
+ * Provides a 'UserProfilBlock' block.
+ *
+ * @Block(
+ *  id = "user_profil_block",
+ *  admin_label = @Translation("User profil block"),
+ * )
+ */
+class UserProfilBlock extends BlockBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $user = $this->routeMatch->getParameter('user');
+
+    $user_picture = UserController::getUserPictureAsRenderableArray($user);
+    $ranking = RankingGeneral::getRankingForBetter($user);
+    return [
+      '#theme' =>'user-block',
+      '#user' => [
+        'name' => $user->getAccountName(),
+        'rank' => $ranking ? $ranking->getPosition() : '/',
+        'points' => $ranking ? $ranking->getPoints() : '/',
+      ],
+      '#links' => [
+        'logout' => Link::fromTextAndUrl(t('Log out'),Url::fromRoute('user.logout',[])),
+        'myaccount' => Link::fromTextAndUrl(t('My account'),Url::fromRoute('entity.user.edit_form',['user'=>$user->id()]))
+      ],
+      '#user_picture' => $user_picture,
+      '#cache' => [
+        'contexts' => ['user'],
+        'tags' => [ 'user:'.$user->id(),'user_block'],
+      ],
+    ];
+  }
+}
