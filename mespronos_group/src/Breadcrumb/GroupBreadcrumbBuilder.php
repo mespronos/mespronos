c35dd9addfdbcccfba25f2fca65aec764aef625a
Kevin Gautreau <contact@kgaut.net>
2016-04-27 15:09:21 +0200

    Group - Breadcrumb

---
+++ mespronos_group/src/Breadcrumb/GroupBreadcrumbBuilder.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\mespronos\Breadcrumb\LeagueBreadcrumbBuilder.
+ */
+
+namespace Drupal\mespronos_group\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Breadcrumb\Breadcrumb;
+
+class GroupBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+    public function applies(RouteMatchInterface $route_match) {
+        return $route_match->getCurrentRouteMatch()->getRouteName() == 'mespronos_group.group';
+    }
+
+    public function build(RouteMatchInterface $route_match) {
+        /** @var \Drupal\mespronos\Entity\Day $day */
+        $breadcrumb = new Breadcrumb();
+        $breadcrumb->addCacheContexts(['route']);
+        $links = [];
+        $links[] = Link::createFromRoute(t('Home'), '<front>');
+        $links[] = Link::createFromRoute(t('Groups'), 'mespronos_group.listing');
+        return $breadcrumb->setLinks($links);
+    }
+
+}
