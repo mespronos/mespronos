9ac692cc2ce2f679e7195508892f38745e3fe9c0
Kevin Gautreau <contact@kgaut.net>
2017-04-15 10:01:34 +0200

    BetBreadcrumb - lint

--- src/Breadcrumb/BetBreadcrumbBuilder.php
+++ src/Breadcrumb/BetBreadcrumbBuilder.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains Drupal\mespronos\Breadcrumb\BetBreadcrumbBuilder.
- */
-
 namespace Drupal\mespronos\Breadcrumb;
 
 use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
@@ -12,23 +7,42 @@ use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Breadcrumb\Breadcrumb;
 
+/**
+ * @file
+ * Contains Drupal\mespronos\Breadcrumb\BetBreadcrumbBuilder.
+ */
+
+/**
+ * Class BetBreadcrumbBuilder.
+ *
+ * @package Drupal\mespronos\Breadcrumb
+ */
 class BetBreadcrumbBuilder implements BreadcrumbBuilderInterface {
 
-    public function applies(RouteMatchInterface $route_match) {
-        return in_array($route_match->getCurrentRouteMatch()->getRouteName(),['mespronos.day.bet','mespronos.lastbetsdetailsforuser']);
-    }
+  /**
+   *
+   * @inheritdoc
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    return in_array($route_match->getCurrentRouteMatch()->getRouteName(),
+      ['mespronos.day.bet', 'mespronos.lastbetsdetailsforuser']);
+  }
 
-    public function build(RouteMatchInterface $route_match) {
-        /** @var \Drupal\mespronos\Entity\Day $day */
-        $day = $route_match->getParameter('day');
-        $league = $day->getLeague();
-        $breadcrumb = new Breadcrumb();
-        $breadcrumb->addCacheContexts(['route']);
-        $links = [];
-        $links[] = Link::createFromRoute(t('Home'), '<front>');
-        $links[] = Link::createFromRoute(t('Leagues'), 'mespronos.leagues.list');
-        $links[] = Link::createFromRoute($league->label(), 'entity.league.canonical',['league'=>$league->id()]);
-        return $breadcrumb->setLinks($links);
-    }
+  /**
+   *
+   * @inheritdoc
+   */
+  public function build(RouteMatchInterface $route_match) {
+    /** @var \Drupal\mespronos\Entity\Day $day */
+    $day = $route_match->getParameter('day');
+    $league = $day->getLeague();
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addCacheContexts(['route']);
+    $links = [];
+    $links[] = Link::createFromRoute(t('Home'), '<front>');
+    $links[] = Link::createFromRoute(t('Leagues'), 'mespronos.leagues.list');
+    $links[] = Link::createFromRoute($league->label(), 'entity.league.canonical', ['league' => $league->id()]);
+    return $breadcrumb->setLinks($links);
+  }
 
 }
