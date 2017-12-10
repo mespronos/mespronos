79ea11830a6efa9a6f765be5a63937cc79ccd292
Kevin Gautreau <contact@kgaut.net>
2017-12-10 17:12:56 +0100

    Lint

--- src/Entity/Interfaces/RankingInterface.php
+++ src/Entity/Interfaces/RankingInterface.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\mespronos\Entity\Interfaces;
 
+use \Drupal\user\Entity\User;
+
 interface RankingInterface {
-  public static function getRankingForBetter(\Drupal\user\Entity\User $better, $entity, $entity_name, $storage_name);
+
+  public static function getRankingForBetter(User $better, $entity, $entity_name, $storage_name);
+
 }
