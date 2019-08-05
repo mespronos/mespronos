7d3f1c31719ee1b401a0de14459488580484d23a
Kevin Gautreau <contact@kgaut.net>
2019-08-05 09:30:49 +0200

    Days - refactoring

--- src/Entity/Day.php
+++ src/Entity/Day.php
@@ -55,8 +55,6 @@ use Drupal\mespronos\Entity\Interfaces\MPNEntityInterface;
  */
 class Day extends MPNContentEntityBase implements MPNEntityInterface {
 
-  use DayGettersTrait;
-
   public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
     parent::preCreate($storage_controller, $values);
     $values += array(
@@ -97,6 +95,104 @@ class Day extends MPNContentEntityBase implements MPNEntityInterface {
         }
       }
     }
+  }
+
+  public function label() {
+    return $this->get('name')->value;
+  }
+
+  public function getRenderableLabel() {
+    $league = $this->getLeague();
+    return [
+      '#theme' => 'day-small',
+      '#league' => [
+        'label' => t($league->label()),
+        'logo' => $league->getLogo('mini_logo'),
+        'url' => $league->url(),
+      ],
+      '#day'=> [
+        'url'=> $this->url(),
+        'label'=> t($this->label()),
+      ]
+    ];
+  }
+
+  /**
+   * @return \Drupal\mespronos\Entity\League
+   */
+  public function getLeague() : League {
+    return $this->get('league')->entity;
+  }
+
+  /**
+   * @return integer
+   */
+  public function getLeagueID() : int {
+    return $this->get('league')->target_id;
+  }
+
+  /**
+   * Return the number of games of the day
+   *
+   * @return int
+   *   Number of games for the day
+   */
+  public function getNbGame() : int {
+    $query = \Drupal::entityQuery('game')->condition('day', $this->id());
+    $ids = $query->execute();
+    return count($ids);
+  }
+
+  /**
+   * Return the day's number
+   *
+   * @return int
+   *   Day's number
+   */
+  public function getDayNumber() : int {
+    return (int) $this->get('number')->target_id;
+  }
+
+  /**
+   * Return all games for day
+   *
+   * @return \Drupal\mespronos\Entity\Game[]
+   */
+  public function getGames($onlyFuture = FALSE) {
+    $ids = $this->getGamesId($onlyFuture);
+    return Game::loadMultiple($ids);
+  }
+
+  /**
+   * Return all games id for day
+   * @return integer[]
+   */
+  public function getGamesId($onlyFuture = FALSE) {
+    $query = \Drupal::entityQuery('game');
+    $query->condition('day', $this->id());
+    if($onlyFuture) {
+      $now = new \DateTime(NULL, new \DateTimeZone('UTC'));
+      $query->condition('game_date', $now->format('Y-m-d\TH:i:s'), '>');
+    }
+    $query->sort('game_date');
+    $query->sort('id');
+
+    return $query->execute();
+  }
+
+  /**
+   * Return the number of games of the day with score setted
+   *
+   * @return int
+   *   Number of games with score setted
+   */
+  public function getNbGameWIthScore() {
+    $query = \Drupal::entityQuery('game')
+      ->condition('day', $this->id())
+      ->condition('score_team_1', NULL, 'IS NOT')
+      ->condition('score_team_2', NULL, 'IS NOT');
+    $ids = $query->execute();
+    return \count($ids);
   }
 
   /**
